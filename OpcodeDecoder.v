3f5b01c321653f31acc913a5f438dfef459f8985
eugene <eugle2000@naver.com>
2024-05-22 13:21:12 +0900

    zz

--- OpcodeDecoder.v
+++ OpcodeDecoder.v
@@ -6,6 +6,12 @@ module OpcodeDecoder(
 
 reg     [3:0]           flag;
 
+parameter load          = 4'b0000,
+          store         = 4'b0001,
+          
+
+
+/*
 parameter JumpAndLink       = 4'b1010,
           LWRegWrite        = 4'b0011,
           SWMemWrite        = 4'b0001,
@@ -25,7 +31,7 @@ always@(*)begin
             flag = 4'b0000; 
     endcase
 end
-
+*/
 assign {jump, flush, RegWrite, MemWrite} = flag;
 assign o_alufunc = i_opcode[1:0]
 
