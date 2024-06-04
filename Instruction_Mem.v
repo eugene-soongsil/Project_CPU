7f1acf5df0df5f87a1b0ef28a250db99e62febd9
eugene <eugle2000@naver.com>
2024-06-05 02:28:22 +0900

    dd

--- Instruction_Mem.v
+++ Instruction_Mem.v
@@ -7,7 +7,7 @@ module Instruction_Mem(
 
     //initial instruction set
     initial begin 
-        instM[0] = 16'b0110_0001_1000_0001; //r1 = r1 + 4
+        instM[0] = 16'b0110_0001_0100_0001; //r1 = r1 + 4
         instM[1] = 16'b0110_0010_0100_0010; //r2 = r2 + 4
         instM[2] = 16'b0110_0011_0001_0011; //r3 = r3 + 1
         instM[3] = 16'b0110_0100_0001_0100; //r4 = r4 + 1
@@ -16,7 +16,7 @@ module Instruction_Mem(
         //if(r1=1)
         instM[6] = 16'b0110_0001_0100_0010; //r2 = r1 + 4
         instM[7] = 16'b0110_0010_0100_0011; //r3 = r2 + 4
-        instM[8] = 16'b0010_0011_0010_0100; //r4 = r1 + r0
+        instM[8] = 16'b0010_0011_0010_0100; //r4 = r3 + r2
     end
 
     assign M_instruction = instM[PCAdd_pc];
