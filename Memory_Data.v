24dc66f51bac361c22b4b08705d78aabd97f771e
eugene <eugle2000@naver.com>
2024-06-09 16:56:19 +0900

    MemInst_final

--- Memory_Data.v
+++ Memory_Data.v
@@ -7,32 +7,15 @@ module MemoryData(
 
 integer i;
 
-reg [15:0] mem[255:0];
+reg [15:0] mem[65536:0];
 
-always@(posedge clk or negedge reset)begin
+always@(negedge clk or negedge reset)begin
     if(~reset)
-        ;
+        for(i=0; i<65536; i=i+1)begin
+            mem[i] <= i;
+        end
     else if(write_en)
         mem[addrM] <= write_dataM;
-end
-
-initial begin
-    mem[0]  <= 16'h1111;
-    mem[1]  <= 16'h1122;
-    mem[2]  <= 16'h2222;
-    mem[3]  <= 16'h2233;
-    mem[4]  <= 16'h3333;
-    mem[5]  <= 16'h3344;
-    mem[6]  <= 16'h4444;
-    mem[7]  <= 16'h4455;
-    mem[8]  <= 16'h5555;
-    mem[9]  <= 16'h5566;
-    mem[10] <= 16'h6666;
-    mem[11] <= 16'h6677;
-    mem[12] <= 16'h7777;
-    mem[13] <= 16'h7788;
-    mem[14] <= 16'h8888;
-    mem[15] <= 16'h8899;
 end
 
 assign read_dataM = mem[addrM];
